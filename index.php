ef9d3b5a7c6cd5bdd7795f72fa69c9d9cf45437e
Felipe0rtiz <89050535+Felipe0rtiz@users.noreply.github.com>
2022-09-21 17:13:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="icon" href="./images/icon.png">
+
+    <script defer src="./js/login.js"></script>
+
+    <title>Memóra Canina | Login</title>
+</head>
+<body>
+
+<form class="login-form">
+
+    <div class="login_header">
+        <img src="./images/capa.jpg" alt="">
+        <h1>Memória Canina</h1>
+    </div>
+
+    <input type="text" placeholder="Nome" class="login_input">
+    <button type="submit" class="login_button" disabled>Começar</button>
+
+</form>
+
+</body>
+</html>
---
+++ pages/game.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/game.css">
+  <link rel="icon" href="../images/icon.png">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <script defer src="../js/game.js"></script>
+
+  <title>Memória Canina</title>
+</head>
+<body>
+  <main>
+    <header>
+      <span class="player"></span> 
+      <span><span class="timer"> 00 </span></span>
+    </header> 
+    <div class="grid"></div>
+    <button type="submit" class="game_button" onclick="window.location.href='/index.html'"><i class="fa fa-home"></i></button>
+  </main>
+
+</body>
+</html>
